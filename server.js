ad2efca85f88ca523d0517881884135f5dd252c5
leptone <leptone55@gmail.com>
2016-04-10 14:01:08 -0700

    fixed server response to AJAX

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var st = ecstatic('public')
 
 var server = http.createServer(function(req, res) {
 	if(req.url === '/carousel-photos') {
+		console.log('in ajax route')
 		fs.readdir('public/img/carousel-images', function(err, images) {
 			if (err) {
 				console.log(err)
 			} else {
-				res.json(images)
+				res.end(JSON.stringify(images))
 			}
 		})
 	}
