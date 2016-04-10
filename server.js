1b1abb812d9170435755663d4bd736c1d04f49a6
leptone <leptone55@gmail.com>
2016-04-10 01:43:17 -0700

    building my own carousel

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+var fs = require('fs')
 var http = require('http')
 var ecstatic = require('ecstatic')
 var st = ecstatic('public')
 
 var server = http.createServer(function(req, res) {
-	st(req, res)
+	if(req.url === '/carousel-photos') {
+		fs.readdir('public/img/carousel-images', function(err, images) {
+			if (err) {
+				console.log(err)
+			} else {
+				res.json(images)
+			}
+		})
+	}
+	else st(req, res)
 })
 
 server.listen(process.env.PORT || 5000)
