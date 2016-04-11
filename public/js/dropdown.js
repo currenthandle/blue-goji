fbd2f17b8248de3d19d6be5a0b1bc5db191096e9
leptone <leptone55@gmail.com>
2016-04-11 12:02:29 -0700

    fixed icon color

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -3,9 +3,12 @@ document.getElementById('dropdown-ul').style.display='none'
 
 dropdown.onclick = function () {
 	var dropdownUl = document.getElementById('dropdown-ul')
+	var dropdownIcon = document.getElementById('dropdown-icon')
 	if (dropdownUl.style.display === 'none') {
 		dropdownUl.style.display = 'inline-block'
+		dropdownIcon.style.color = '#5ac40f'
 	} else {
 		dropdownUl.style.display = 'none'
+		dropdownIcon.style.color = '#0eb5fe'
 	}
 }
